9ff093c045db2bb31f195f09fb61aaf0687bf88a
agraham <agraham0925@gmail.com>
2018-03-30 11:49:45 -0500

    created placeholder images for bucket and kernels

--- js/app.js
+++ js/app.js
@@ -1,21 +1,42 @@
 console.log('test test')
 
+//GAME PLANNING:
+	//OUTCOME ONE: you collect popcorn kernel
+	//OUTCOME TWO: you collect unpopped kernel
+	//OUTCOME THREE: you miss popcorn kernel
+	//OUTCOME FOUR: you miss unpopped kernel
+
+
 //STEPS TO BUILDING GAME
 
-//obejects:
+//objects:
 	//popcorn kernels
+
 	//unpopped kernels
 		//function to generate random point value
+	class UnpoppedKernel {
+		constructor() {
+			let pointValues = [1, 2, 3];
+			const points = pointValues[Math.floor(Math.random() * pointValues.length)];
+			this.points = points;
+		}	
+	}
+	
 	//bucket
+	
 	//player 1
 		//total score
 		//current score
+	
 	//player 2
 		//total score
 		//current score
+	
 	//scoreboard
+	
 	//timer
 
 //NICE TO HAVES
 	//popcorn animation for winner
 	//different color buckets for each player
+	//top level where unpopped turns into popped
