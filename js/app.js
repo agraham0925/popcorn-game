50a3d9aabd647772f6c3d305aac253dcbd6aef45
agraham <agraham0925@gmail.com>
2018-03-30 12:31:13 -0500

    created function for creating popcorn

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ console.log('test test')
 const game = {
 	kernels: [],
 	currentKernel: null,
+	popcorn: [],
+	currentPopcorn: null,
 	makeUnpopped() {
 		for(let i = 0; i < 6; i++) {
 			const unpoppedK = new UnpoppedKernel();
@@ -16,7 +18,21 @@ const game = {
 	},
 	getKernel() {
 		this.currentKernel = this.kernels.pop();
+		console.log(this.kernels)
 		console.log(this.currentKernel)
+	},
+	makePopcorn() {
+		for(let i = 0; i < 6; i++) {
+			const popcornPiece = new Popcorn();
+			this.popcorn.push(popcornPiece);
+			console.log(this.popcorn)
+			console.log(this.currentPopcorn)
+		}
+	},
+		getPopcorn() {
+		this.currentPopcorn = this.popcorn.pop();
+		console.log(this.popcorn)
+		console.log(this.currentPopcorn)
 	}
 }
 
@@ -25,6 +41,11 @@ const game = {
 
 //objects:
 	//popcorn kernels
+class Popcorn {
+	constructor() {
+		this.points = 1;
+	}
+}
 
 //unpopped kernels
 	//function to generate random point value
